7bc460a0c11e679aef2961c4ed1df2b774576f6f
almazbekovna08 <themoongirl234@gmail.com>
2025-03-12 16:22:17 +0600

    hw6

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello world')
+def enter_info(list_of_info):
+        info = input("Введите любые данные: ")
+        list_of_info.append(info)
+
+def get_info(list_of_info):
+    print(f"Ваш список данных: {list_of_info}")
+
+def main():
+    list_of_info=[]
+    enter_info(list_of_info)
+    get_info(list_of_info)
+main()
